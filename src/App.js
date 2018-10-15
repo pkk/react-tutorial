7881d06fd040ed7199b53fcf9d15620202d892ac
kpalakaluri <kpalakaluri@hypergrid.com>
2018-10-16 00:35:53 +0530

    Added submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React, {Component} from 'react';
 import Table from './Table';
+import Form from './Form';
+
+
 class App extends Component {
 	state = {
-		characters: [
-			{
-				'name': 'Charlie',
-				'job': 'Janitor'
-			},
-			{
-				'name': 'Mac',
-				'job': 'Bouncer'
-			},
-			{
-				'name': 'Dee',
-				'job': 'Aspiring actress'
-			},
-			{
-				'name': 'Dennis',
-				'job': 'Bartender'
-			}
-		]
+		characters: []
 	};
+
 	removeCharacter = index => {
 		const { characters } = this.state;
 
@@ -29,15 +16,20 @@ class App extends Component {
 				return i !== index;
 			})
 		});
-	}
+	};
+	handleSubmit = character => {
+	        this.setState({characters: [...this.state.characters, character]});
+
+	};
 	render() {
 		const { characters } = this.state;
 		return (
 			<div className="container">
 				<Table 
-					characterData={characters}
+					characterData={this.state.characters}
 					removeCharacter={this.removeCharacter}
 				/>
+				<Form 	handleSubmit={this.handleSubmit}/>
 			</div>
 		);
 	};
